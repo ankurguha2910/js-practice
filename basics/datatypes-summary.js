412acf0a92400ccff7af15ead8813512661fb294
Ankur Guha <148000596+ankurguha2910@users.noreply.github.com>
2024-02-20 12:07:24 +0000

    memory

--- basics/datatypes-summary.js
+++ basics/datatypes-summary.js
@@ -50,3 +50,33 @@ console.log(myObj);
 
 console.log(typeof myFunction);
 console.log(myFunction);
+
+console.log('------------------------------------------------');
+//+++++++++++++++++++++Memory+++++++++++++++++++++++++++++++
+// Stack memory(used for primitive data types), Heap memory (used for non-primitive data types)
+
+let myName = "ankur"
+
+let anotherName = myName
+
+anotherName = "gogol" 
+
+//since primitive data types sends a copy of the original memory that is why any modificaton done is only reflected on the copied instance and not on the original memory instance.
+
+console.log(myName); //the original value remains the same
+console.log(anotherName); // the modified value is reflected 
+
+//Since non-primitive data types stores data in heap memory only reference is returned, thus multple instances of stack if points to the same data as in the heap, then it points to is's reference and not it's copy unlike primitive data types. Hence any changes made is directly reflected on the actual data.
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl.com"
+}
+
+let userTwo = userOne
+
+userTwo.email = "ankur@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+console.log(userOne);
