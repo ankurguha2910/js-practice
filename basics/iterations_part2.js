5706a509c0d718b94eda22b65bf327e6b2631fa0
Ankur Guha <148000596+ankurguha2910@users.noreply.github.com>
2024-02-26 18:55:22 +0000

    whle and do-while loop

---
+++ basics/iterations_part2.js
@@ -0,0 +1,25 @@
+let i = 0
+while(i <= 30){
+    console.log(`Value of i is ${i}`);
+    i = i+3
+}
+
+console.log("-----------------------------------");
+
+let myArr = ["superman", "spiderman", "batman"]
+
+let index = 0
+while(index < myArr.length)
+{
+    console.log(`Value is ${myArr[index]}`);
+    index++
+}
+
+console.log("-----------------------------------");
+//do-while loop
+let score = 1
+
+do{
+    console.log(`Value is ${score}`);
+    score++
+} while(score <= 10)
